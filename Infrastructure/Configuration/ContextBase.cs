1b7da97b4585fcaa6a3f04164aab410eb308e431
Marcos Keller <marcoskeller@hotmail.com>
2022-10-20 19:24:31 -0300

    Criacao dos repositorios na Camada de Infraestrutura

---
+++ Infrastructure/Configuration/ContextBase.cs
@@ -0,0 +1,45 @@
+﻿using Entities.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Configuration
+{
+    public class ContextBase : IdentityDbContext<ApplicationUser>
+    {
+
+
+        public ContextBase(DbContextOptions<ContextBase> options) : base(options)
+        {
+
+        }
+
+
+        public DbSet<Message> Message { get; set; }
+        public DbSet<ApplicationUser> ApplicationUser { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(ObterStringConexao());
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ApplicationUser>().ToTable("AspNetUsers").HasKey(t => t.Id);
+
+            base.OnModelCreating(builder);
+        }
+
+
+        //Método alternativo para obter a string de conexão com o banco de dados caso não exista no arquivo appSettings.json
+        public string ObterStringConexao()
+        {
+            return "Data Source=caminhoParaAcessarBanco;Initial Catalog=nomeDoBanco;Integrated Security=False;User ID=usuarioDoBanco;Password=senhaDoBanco;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False";
+        }
+    }
+}
---
+++ Infrastructure/Repository/Repositories/RepositoryMessage.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Interfaces;
+using Entities.Entities;
+using Infrastructure.Configuration;
+using Infrastructure.Repository.Generics;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repository.Repositories
+{
+    public class RepositoryMessage : RepositoryGenerics<Message>, IMessage
+    {
+
+        private readonly DbContextOptions<ContextBase> _optionsBuilder;
+
+        public RepositoryMessage()
+        {
+            _optionsBuilder = new DbContextOptions<ContextBase>();
+        }
+    }
+}
