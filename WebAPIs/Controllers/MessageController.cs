0c4b83c50d3f9e0a26e593fb14b6909efce57d70
Marcos Keller <marcoskeller@hotmail.com>
2022-10-24 12:15:14 -0300

    Criacao da Controller Message

---
+++ WebAPIs/Controllers/MessageController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPIs.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+
+    }
+}
